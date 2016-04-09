c4f31746b14a40e61b33d50e62be22c607c6b5dc
Paweł Rother <pawerot276@student.polsl.pl>
2016-04-09 11:24:05 +0200

    lab

---
+++ 2016_03_19/2016_03_19/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016_03_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string napis;
+            int cos = 100;
+            napis = "Hello world";
+            System.String napis2;
+            napis2 = "My first program on c#";
+            Console.WriteLine(napis+ " " + cos);
+            
+           string txt = Console.ReadLine();*/
+
+            /*int x, y;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("suma: {0} \niloczyn: {1}", y + x, y * x);
+            if (x + y % 2 == 0)
+                Console.WriteLine("suma jest przysta");
+            else
+                Console.WriteLine("suma jest nieparzysta");*/
+
+            /* int a = int.Parse(Console.ReadLine());
+             if (a<53)
+             {
+                 Console.WriteLine("liczba: {0}", a);
+                 if (a >= 0)
+                     Console.WriteLine("liczba nieujemna");
+                 else
+                     Console.WriteLine("liczba niedodatnia");*/
+
+            double x, y, z;
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            z = double.Parse(Console.ReadLine());
+
+            if (x == 0)
+                Console.WriteLine("to nie jest rownanie kwadratowe");
+            else
+            {
+                Console.WriteLine("rownianie ma postac:  {0}x^2 + {1}x + {2} = 0");
+                double delta = 
+            }
+        }
+    }
+}
---
+++ 2016_04_09/2016_04_09/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016_04_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] tab = new int[30];
+            Random R = new Random();
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = R.Next(0, 100);
+            }
+
+            int u = 0;
+
+            //wypiswanie
+            foreach (var i in tab)
+            {
+                System.Console.WriteLine("Tablica[{0}]={1}", u, i);
+                u++;
+            }
+
+            //max
+            int max = tab[0];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] > max)
+                    max = tab[i];
+            }
+            Console.WriteLine("MAX={0}", max);
+
+            //suma
+            int suma = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                suma = suma + tab[i];
+            }
+            Console.WriteLine("suma={0}", suma);        
+
+            int x = 72;
+            int y = 48;
+            while (x != y)
+            {
+                if (x > y)
+                    x = x - y;
+                else
+                    y = y - x;
+            }
+            Console.WriteLine("NWD={0}", x);
+
+
+            sort(tab);
+
+            //wypisz
+            for (int i = 0; i < tab.Length; i++)
+            {
+                System.Console.WriteLine("Tablica[{0}]={1}", i, tab[i]);
+            }
+
+        }
+        static void sort(int[] tab)
+        {
+            int x, j;
+            for (int i = 1; i < tab.Length; i++)
+            {
+                x = tab[i];
+                j = i - 1;
+                while (j > 0 && x < tab[j])
+                {
+                    tab[j + 1] = tab[j];
+                    j = j - 1;
+                }
+                tab[j + 1] = x;
+            }
+        }
+    }
+}
